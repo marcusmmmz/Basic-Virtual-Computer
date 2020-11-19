089757129900ae381f66e6e9ba54c906018d1819
marcusmmmz <marcusizabel230@gmail.com>
2020-11-19 22:04:35 +0000

    Binary instructions

--- index.js
+++ index.js
@@ -2,51 +2,52 @@ let debug = false
 
 const isNode = (typeof window === 'undefined')
 
-function range(size, startAt = 0) {
-    return [...Array(size).keys()].map(i => i + startAt);
-}
-
 let instructions = {
-    "LOAD": 0b0000,
-    "ADD":  0b0001,
-    "STORE":0b0010,
-    "JUMP": 0b0011,
+    "LOAD": 0b0001,
+    "ADD":  0b0010,
+    "STORE":0b0011,
+    "JUMP": 0b0100,
 }
 
 //fetch, decode, execute
 let CPU = {
-    clock : 1000, //CPU Clock (Milliseconds)
-    counter:0, //Current RAM address
-    register:{}, //Instruction register
-    accumulator:null,
+    clock : 500, //CPU Clock (Milliseconds)
+    address:0, //Current RAM address
+    register:0, //Instruction register
+    accumulator:0,
 
+    instruction:0, //1st part of register
+    argument:0, //2nd part of register
     fetch() {
-        this.register = RAM.adresses[this.counter]
+        this.register = RAM.adresses[this.address]
     },
     decode() {
-
+        this.instruction = this.register >> 4
+        this.argument = this.register - (this.instruction << 4)
     },
     execute() {
-
-        switch (this.register.instruction) {
+        let instruction = this.instruction
+        let argument = this.argument
+        
+        switch (instruction) {
             case instructions["LOAD"]:
-                if (debug) console.log(`Loading ${RAM.adresses[this.register.argument]} from address ${this.register.argument}`)
-                this.accumulator = RAM.adresses[this.register.argument]
+                if (debug) console.log(`Loading ${RAM.adresses[argument]} from address ${argument}`)
+                this.accumulator = RAM.adresses[argument]
                 break;
         
             case instructions["ADD"]:
-                if (debug) console.log(`Adding ${this.accumulator} from address ${this.register.argument}`)
-                this.accumulator += RAM.adresses[this.register.argument]
+                if (debug) console.log(`Adding ${this.accumulator} from address ${argument}`)
+                this.accumulator += RAM.adresses[argument]
                 break;
     
             case instructions["STORE"]:
-                if (debug) console.log(`Storing ${this.accumulator} at address ${this.register.argument}`)
-                RAM.adresses[this.register.argument] = this.accumulator
+                if (debug) console.log(`Storing ${this.accumulator} at address ${argument}`)
+                RAM.adresses[argument] = this.accumulator
                 break;
     
             case instructions["JUMP"]:
-                if (debug) console.log(`Jumping to address ${this.register.argument}`)
-                this.counter = this.register.argument
+                if (debug) console.log(`Jumping to address ${argument}`)
+                this.address = argument
                 break;
 
             default:
@@ -54,37 +55,16 @@ let CPU = {
                 break;
         }
 
-        
     },
     cycle() {
         this.fetch()
         this.decode()
         this.execute()
-        if (this.register.instruction != instructions["JUMP"]) {
-            this.counter += 1
+        if (this.instruction != instructions["JUMP"]) {
+            this.address += 1
         }
 
-        if (!isNode) {
-            let output = ""
-            RAM.adresses.forEach((value, i, arr) => {
-                if (value.instruction != undefined) {
-                    output += `Address ${i}: ${encodeInstruction(value.instruction)} ${value.argument} <br/>`
-                } else {
-                    output += `Address ${i}: ${value.toString()} <br/>`
-                }
-            })
-
-            document.getElementById("addresses").innerHTML = output
-
-            output = ""
-
-            output += `clock: ${this.clock} <br/>`
-            output += `counter: ${this.counter} <br/>`
-            output += `register: ${encodeInstruction(this.register.instruction)} ${this.register.argument} <br/>`
-            output += `accumulator: ${this.accumulator} <br/>`
-
-            document.getElementById("cpu").innerHTML = output
-        }
+        updateGUI()
 
     },
 
@@ -100,13 +80,10 @@ let CPU = {
 }
 
 function instruction(instruction, argument) {
-    return {
-        instruction:instructions[instruction],
-        argument
-    }
+    return (instructions[instruction] << 4) + argument
 }
 
-function encodeInstruction(number) {
+function displayInstruction(number) {
     for (const instruction in instructions) {
         if (instructions.hasOwnProperty(instruction)) {
             const element = instructions[instruction];
@@ -130,4 +107,43 @@ let RAM = {
     ],
 }
 
-CPU.start()
+function updateGUI() {
+    if (!isNode) {
+        let output = ""
+
+        for (const instruction in instructions) {
+            if (instructions.hasOwnProperty(instruction)) {
+                const element = instructions[instruction];
+                output += `${instruction} : ${element} <br/>`
+            }
+        }
+
+        document.getElementById("instructions").innerHTML = output
+
+        output = ""
+
+        RAM.adresses.forEach((value, i, arr) => {
+            if (value >> 4) {
+                output += `Address ${i}: ${displayInstruction(value >> 4)} ${value - (value >> 4 << 4)} <br/>`
+            } else {
+                output += `Address ${i}: ${value} <br/>`
+            }
+        })
+
+        document.getElementById("addresses").innerHTML = output
+
+        output = ""
+
+        output += `Clock: ${CPU.clock} <br/>`
+        output += `Address: ${CPU.address} <br/>`
+        output += `Register: ${
+            displayInstruction(CPU.instruction) || "NULL"
+        } ${
+            CPU.argument
+        } <br/>`
+        output += `Accumulator: ${CPU.accumulator} <br/>`
+
+        document.getElementById("cpu").innerHTML = output
+    }
+}
+updateGUI()
